cf2a00ac02d6028921c5bc8b7b854639e998b69f
Penha Vasyl <vasylpenha@gmail.com>
2021-02-18 18:53:47 +0200

    Correct variable names,fix mistakes

--- main.js
+++ main.js
@@ -3,44 +3,40 @@ const headphoneKoss = 123.965;
 const headphoneSennheiser = 90.2345;
 
 // Використовуючи вбудований об'єкт Math – виведіть максимальне число
-const maxNumber = Math.max(headphoneSven, headphoneKoss, headphoneSennheiser);
-console.log(maxNumber);
+const maxHeadphonesPrice = Math.max(headphoneSven, headphoneKoss, headphoneSennheiser);
+console.log(maxHeadphonesPrice);
 
 // Використовуючи вбудований об'єкт Math – виведіть мінімальне число
-const minNumber = Math.min(headphoneSven, headphoneKoss, headphoneSennheiser);
-console.log(minNumber);
+const minHeadphonesPrice = Math.min(headphoneSven, headphoneKoss, headphoneSennheiser);
+console.log(minHeadphonesPrice);
 
 // Складіть вартість всіх товарів, помістіть її в змінну та виведіть цю суму
-const price = headphoneSven + headphoneKoss + headphoneSennheiser;
-console.log(price);
+const costOfAllGoods = headphoneSven + headphoneKoss + headphoneSennheiser;
+console.log(costOfAllGoods);
 
 // Відкиньте копійки у всіх товарів, потім – складіть цілу частину вартості кожного товару між собою. 
 // Округлення використовувати в МЕНЬШУ сторону.
-const priceRound = Math.floor(headphoneSven) + Math.floor(headphoneKoss) + Math.floor(headphoneSennheiser);
-console.log(priceRound);
+const roundedCostOfAllGoods = Math.floor(headphoneSven) + Math.floor(headphoneKoss) + Math.floor(headphoneSennheiser);
+console.log(roundedCostOfAllGoods);
 
 // Виведіть суму товарів округлену до сотень. (Наприклад якщо вийшло 260, то виведіть 300)
 function roundToMultiple(num, multiple) {
   return Math.round(num / multiple) * multiple;
 }
-const priceRoundedToHundred = (roundToMultiple(headphoneSven + headphoneKoss + headphoneSennheiser, 100));
+const priceRoundedToHundred = (roundToMultiple(costOfAllGoods, 100));
 console.log(priceRoundedToHundred);
 
 // Виведіть булеве значення: чи є сума всіх товарів (округлена в меншу сторону) парним чи непарним числом?
-if(priceRound % 2 == 0) {
-  console.log(true);
-}
-else {
-  console.log(false);
-}
+const evenOrOddNumber = roundedCostOfAllGoods % 2 === 0;
+console.log(evenOrOddNumber);
 
 // Виведіть суму решти, при оплаті всіх товарів (без округлення), якщо клієнт платить 500 грн.
 const payment = 500;
-const restMoney = Number((payment - price).toFixed(2));
-console.log(restMoney);
+const restOfMoney = + ((payment - costOfAllGoods).toFixed(2));
+console.log(restOfMoney);
 
 // Виведіть середнє значення цін, округлене до другого знаку після коми
-const averagePrice = Number(((headphoneSven + headphoneKoss + headphoneSennheiser)/3).toFixed(2));
+const averagePrice = + (((headphoneSven + headphoneKoss + headphoneSennheiser)/3).toFixed(2));
 console.log(averagePrice);
 
 // (Більш складне) Створіть змінну, в якої збережіть випадкову знижку (використовуйте функцію Math.random).
@@ -49,22 +45,23 @@ console.log(averagePrice);
 
 const discount = Math.round(Math.random() * 100);
 console.log('Знижка =', discount);
-const amountForPayment = Number((price - (price * (discount / 100))).toFixed(2));
+const amountForPayment = + ((costOfAllGoods - (costOfAllGoods * (discount / 100))).toFixed(2));
 console.log('Сума до оплати = ',amountForPayment);
-const profit = Number((amountForPayment - price / 2).toFixed(2));
+const profit = + ((amountForPayment - costOfAllGoods / 2).toFixed(2));
 console.log('Чистий прибуток = ', profit);
 
 // Advanced
 const answers = `\n 
-Mаксимальне число = ${maxNumber}
-Mінімальне число = ${minNumber}
-Вартість всіх товарів = ${price}
-Округлена вартість товарів = ${priceRound}
-Округлена вартість товарів до сотень = ${priceRoundedToHundred}
-Cуму решти = ${restMoney}
-Cереднє значення цін, округлене до другого знаку після коми = ${averagePrice}
-Знижка = ${discount}
-Сума до оплати = ${amountForPayment}
-Чистий прибуток = ${profit}`
+  Mаксимальне число = ${maxHeadphonesPrice}
+  Mінімальне число = ${minHeadphonesPrice}
+  Вартість всіх товарів = ${costOfAllGoods}
+  Округлена вартість товарів = ${roundedCostOfAllGoods}
+  Округлена вартість товарів до сотень = ${priceRoundedToHundred}
+  Чи парне число = ${evenOrOddNumber}
+  Cуму решти = ${restOfMoney}
+  Cереднє значення цін, округлене до другого знаку після коми = ${averagePrice}
+  Знижка = ${discount}
+  Сума до оплати = ${amountForPayment}
+  Чистий прибуток = ${profit}`;
 
 console.log(answers);
